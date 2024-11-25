4985c8049cf63bb3ef1a515df35d667770f1b783
HyperWinX <artmsmth@gmail.com>
2024-11-25 08:24:12 +0400

    Disabled LTO

--- cmake/Variables.cmake
+++ cmake/Variables.cmake
@@ -2,4 +2,4 @@ set(FAST_COMPILE_FLAGS "-Wall -Wextra -Wno-pointer-arith -O3 -Wno-unused-const-v
 set(DEBUG_COMPILE_FLAGS "-Wall -Wextra -Werror -Wno-pointer-arith -O0 -ggdb3 -Wno-unused-const-variable -fno-sanitize-recover=all -fsanitize=address,undefined")
 
 set(TESTING_COMPILE_FLAGS_NOOPT "-Wall -Wextra -Werror -Wno-pointer-arith -O0 -ggdb3 -Wno-unused-const-variable -fno-sanitize-recover=all -fsanitize=address,undefined")
-set(TESTING_COMPILE_FLAGS_ALLOPT "-Wall -Wextra -Werror -Wno-pointer-arith -O0 -ggdb3 -Wno-unused-const-variable -fno-sanitize-recover=all -fsanitize=address,undefined -flto=thin")
+set(TESTING_COMPILE_FLAGS_ALLOPT "-Wall -Wextra -Werror -Wno-pointer-arith -O3 -ggdb3 -Wno-unused-const-variable -fno-sanitize-recover=all -fsanitize=address,undefined")
