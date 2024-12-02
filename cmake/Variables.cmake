2f27d3d034af9d7f47ea144c63cf4bfa4b483bff
HyperWinX <artmsmth@gmail.com>
2024-12-02 08:50:32 +0400

    Fixed RM_R tests

--- cmake/Variables.cmake
+++ cmake/Variables.cmake
@@ -1,5 +1,5 @@
 set(FAST_COMPILE_FLAGS "-Wall -Wextra -Wno-pointer-arith -O3 -Wno-unused-const-variable")
-set(DEBUG_COMPILE_FLAGS "-Wall -Wextra -Werror -Wno-pointer-arith -O0 -ggdb3 -Wno-unused-const-variable -fno-sanitize-recover=all -fsanitize=address,undefined")
+set(DEBUG_COMPILE_FLAGS "-Wall -Wextra -Werror -Wno-pointer-arith -O0 -ggdb3 -Wno-unused-const-variable")
 
-set(TESTING_COMPILE_FLAGS_NOOPT "-Wall -Wextra -Werror -Wno-pointer-arith -O0 -ggdb3 -Wno-unused-const-variable -fno-sanitize-recover=all -fsanitize=address,undefined")
-set(TESTING_COMPILE_FLAGS_ALLOPT "-Wall -Wextra -Werror -Wno-pointer-arith -O3 -ggdb3 -Wno-unused-const-variable -fno-sanitize-recover=all -fsanitize=address,undefined")
+set(TESTING_COMPILE_FLAGS_NOOPT "-Wall -Wextra -Werror -Wno-pointer-arith -O0 -ggdb3 -Wno-unused-const-variable")
+set(TESTING_COMPILE_FLAGS_ALLOPT "-Wall -Wextra -Werror -Wno-pointer-arith -O3 -ggdb3 -Wno-unused-const-variable")
--- test/integration/core/cpu/mov_instr/test_cpu_mov.cpp
+++ test/integration/core/cpu/mov_instr/test_cpu_mov.cpp
@@ -244,7 +244,8 @@ TEST_F(cpu_test, INSTR_MOV_RM_R_b8) {
 TEST_F(cpu_test, INSTR_MOV_RM_R_b16) {
   cpu.mem_controller->load16(*cpu.xip, hypercpu::opcode::MOV);
   cpu.mem_controller->load8(*cpu.xip + 2, hypercpu::mode::b16 << 6 | hypercpu::operand_types::RM_R);
-  cpu.mem_controller->load16(*cpu.xip + 3, hypercpu::registers::X0 << 8 | hypercpu::registers::XLL1);
+  cpu.mem_controller->load8(*cpu.xip + 3, hypercpu::registers::X0);
+  cpu.mem_controller->load8(*cpu.xip + 4, hypercpu::registers::XLL1);
   cpu.mem_controller->load16(*cpu.xip + 5, hypercpu::opcode::HALT);
   cpu.mem_controller->load8(*cpu.xip + 7, hypercpu::operand_types::NONE);
   *cpu.x0 = 1024;
@@ -258,7 +259,8 @@ TEST_F(cpu_test, INSTR_MOV_RM_R_b16) {
 TEST_F(cpu_test, INSTR_MOV_RM_R_b32) {
   cpu.mem_controller->load16(*cpu.xip, hypercpu::opcode::MOV);
   cpu.mem_controller->load8(*cpu.xip + 2, hypercpu::mode::b32 << 6 | hypercpu::operand_types::RM_R);
-  cpu.mem_controller->load16(*cpu.xip + 3, hypercpu::registers::X0 << 8 | hypercpu::registers::XL1);
+  cpu.mem_controller->load8(*cpu.xip + 3, hypercpu::registers::X0);
+  cpu.mem_controller->load8(*cpu.xip + 4, hypercpu::registers::XL1);
   cpu.mem_controller->load16(*cpu.xip + 5, hypercpu::opcode::HALT);
   cpu.mem_controller->load8(*cpu.xip + 7, hypercpu::operand_types::NONE);
   *cpu.x0 = 1024;
@@ -272,7 +274,8 @@ TEST_F(cpu_test, INSTR_MOV_RM_R_b32) {
 TEST_F(cpu_test, INSTR_MOV_RM_R_b64) {
   cpu.mem_controller->load16(*cpu.xip, hypercpu::opcode::MOV);
   cpu.mem_controller->load8(*cpu.xip + 2, hypercpu::mode::b64 << 6 | hypercpu::operand_types::RM_R);
-  cpu.mem_controller->load16(*cpu.xip + 3, hypercpu::registers::X0 << 8 | hypercpu::registers::X1);
+  cpu.mem_controller->load8(*cpu.xip + 3, hypercpu::registers::X0);
+  cpu.mem_controller->load8(*cpu.xip + 4, hypercpu::registers::X1);
   cpu.mem_controller->load16(*cpu.xip + 5, hypercpu::opcode::HALT);
   cpu.mem_controller->load8(*cpu.xip + 7, hypercpu::operand_types::NONE);
   *cpu.x0 = 1024;
@@ -281,4 +284,19 @@ TEST_F(cpu_test, INSTR_MOV_RM_R_b64) {
   cpu.run();
 
   ASSERT_EQ(cpu.mem_controller->read64(1024), static_cast<std::uint64_t>(0x5555555555555555));
+}
+
+TEST_F(cpu_test, INSTR_MOV_RM_M_b8) {
+  cpu.mem_controller->load16(*cpu.xip, hypercpu::opcode::MOV);
+  cpu.mem_controller->load8(*cpu.xip + 2, hypercpu::mode::b8 << 6 | hypercpu::operand_types::RM_M);
+  cpu.mem_controller->load8(*cpu.xip + 3, hypercpu::registers::X0);
+  cpu.mem_controller->load64(*cpu.xip + 4, 1536);
+  cpu.mem_controller->load16(*cpu.xip + 12, hypercpu::opcode::HALT);
+  cpu.mem_controller->load8(*cpu.xip + 14, hypercpu::operand_types::NONE);
+  cpu.mem_controller->load8(1536, 0x55);
+  *cpu.x0 = 1024;
+
+  cpu.run();
+
+  ASSERT_EQ(cpu.mem_controller->read64(1024), static_cast<std::uint8_t>(0x55));
 }
