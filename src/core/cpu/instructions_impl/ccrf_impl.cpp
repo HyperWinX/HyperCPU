e65ae7e4d73dbf07a495d7edbe4a62d726706605
HyperWinX <artmsmth@gmail.com>
2024-12-16 09:50:30 +0400

    Fixed naming

--- src/core/cpu/instructions_impl/ccrf_impl.cpp
+++ src/core/cpu/instructions_impl/ccrf_impl.cpp
@@ -5,7 +5,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-void hypercpu::cpu::exec_call(operand_types op_types, mode md, void* op1, void* op2) {
+void hypercpu::cpu::exec_ccrf(operand_types op_types, mode md, void* op1, void* op2) {
   crf = false;
 }
 
