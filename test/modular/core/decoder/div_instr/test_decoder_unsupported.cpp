341200b4395d530acbd2644807734809be16c27c
HyperWinX <artmsmth@gmail.com>
2024-12-24 13:44:07 +0000

    Fixed decoder tests

--- test/modular/core/decoder/div_instr/test_decoder_unsupported.cpp
+++ test/modular/core/decoder/div_instr/test_decoder_unsupported.cpp
@@ -4,6 +4,50 @@
 
 #include <fixtures.hpp>
 
+TEST_F(decoder_test, DIV_INSTR_IMM_B8) {
+  decoder.mem_controller->load16(counter, hypercpu::DIV);
+  counter += 2;
+  decoder.mem_controller->load8(counter, (hypercpu::mode::b8 << 6) | hypercpu::operand_types::IMM);
+  ++counter;
+  decoder.mem_controller->load8(counter, 0x55);
+  counter = 0;
+  
+  ASSERT_EXIT(decoder.fetch_and_decode(), ::testing::ExitedWithCode(1), "Invalid operation mode for this opcode!");
+}
+
+TEST_F(decoder_test, DIV_INSTR_IMM_B16) {
+  decoder.mem_controller->load16(counter, hypercpu::DIV);
+  counter += 2;
+  decoder.mem_controller->load8(counter, (hypercpu::mode::b16 << 6) | hypercpu::operand_types::IMM);
+  ++counter;
+  decoder.mem_controller->load16(counter, 0x5555);
+  counter = 0;
+  
+  ASSERT_EXIT(decoder.fetch_and_decode(), ::testing::ExitedWithCode(1), "Invalid operation mode for this opcode!");
+}
+
+TEST_F(decoder_test, DIV_INSTR_IMM_B32) {
+  decoder.mem_controller->load16(counter, hypercpu::DIV);
+  counter += 2;
+  decoder.mem_controller->load8(counter, (hypercpu::mode::b32 << 6) | hypercpu::operand_types::IMM);
+  ++counter;
+  decoder.mem_controller->load32(counter, 0x55555555);
+  counter = 0;
+
+  ASSERT_EXIT(decoder.fetch_and_decode(), ::testing::ExitedWithCode(1), "Invalid operation mode for this opcode!");
+}
+
+TEST_F(decoder_test, DIV_INSTR_IMM_B64) {
+  decoder.mem_controller->load16(counter, hypercpu::DIV);
+  counter += 2;
+  decoder.mem_controller->load8(counter, (hypercpu::mode::b64 << 6) | hypercpu::operand_types::IMM);
+  ++counter;
+  decoder.mem_controller->load64(counter, 0x5555555555555555);
+  counter = 0;
+
+  ASSERT_EXIT(decoder.fetch_and_decode(), ::testing::ExitedWithCode(1), "Invalid operation mode for this opcode!");
+}
+
 TEST_F(decoder_test, DIV_INSTR_R_R_B8) {
   decoder.mem_controller->load16(counter, hypercpu::DIV);
   counter += 3;
