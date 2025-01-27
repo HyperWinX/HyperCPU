888cebab9a1d4e12260240d509f01357419c6886
HyperWinX <artmsmth@gmail.com>
2025-01-27 23:57:17 +0400

    Updated workflow

--- test/Modular/EmulatorCore/Decoding/READInstr/IMM.cpp
+++ test/Modular/EmulatorCore/Decoding/READInstr/IMM.cpp
@@ -1,6 +1,6 @@
 #include <fixtures.hpp>
 
-TEST_F(DECODER_TEST, WRITE_INSTR_IMM) {
+TEST_F(DECODER_TEST, READ_INSTR_IMM) {
   decoder.mem_controller->Load16(counter, HyperCPU::WRITE);
   counter += 2;
   decoder.mem_controller->Load8(counter, HyperCPU::OperandTypes::IMM);
