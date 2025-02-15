2f8f8823b93841a3c7efe3dbd021a253d319e1d5
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-02-15 22:28:11 +0300

    Upload "Hello, world!" example code file

---
+++ examples/hello_world.s
@@ -0,0 +1,16 @@
+main:
+	write xlll0, 'H';
+	write xlll0, 'e';
+	write xlll0, 'l';
+	write xlll0, 'l';
+	write xlll0, 'o';
+	write xlll0, ',';
+	write xlll0, ' ';
+	write xlll0, 'w';
+	write xlll0, 'o';
+	write xlll0, 'r';
+	write xlll0, 'l';
+	write xlll0, 'd';
+	write xlll0, '!';
+	write xlll0, 0u10;
+	halt;
