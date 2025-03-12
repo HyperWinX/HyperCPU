8e332fb98e99f29f3cdf5cb92e9ba093e3562b62
HyperWinX <artmsmth@gmail.com>
2025-03-12 21:57:43 +0400

    Fixed UB in custom bit_cast

--- src/Emulator/Misc/bit_cast.hpp
+++ src/Emulator/Misc/bit_cast.hpp
@@ -3,12 +3,14 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <algorithm>
+
 
 namespace HyperCPU {
   template<typename To, typename From>
   constexpr To bit_cast(const From& src) noexcept {
     To dst;
-    std::memcpy(&dst, &src, sizeof(To));
+    std::memcpy(&dst, &src, std::min(sizeof(To), sizeof(From)));
     return dst;
   }
 
