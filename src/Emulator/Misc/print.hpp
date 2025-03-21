54f245fdffe84bf7ddfefca5281e6475b2c7c867
HyperWinX <artmsmth@gmail.com>
2025-03-21 15:46:54 +0400

    Fix logging formatting

--- src/Emulator/Misc/print.hpp
+++ src/Emulator/Misc/print.hpp
@@ -5,12 +5,12 @@
 
 namespace HyperCPU {
   template<typename... Args>
-  constexpr void print(std::format_string<Args...> form, Args&&... args) {
-    std::cout << std::format(form, std::forward<Args>(args)...);
+  constexpr void print(std::string_view form, Args... args) {
+    std::cout << std::vformat(form, std::make_format_args(args...));
   }
 
   template<typename... Args>
-    constexpr void println(std::format_string<Args...> form, Args&&... args) {
-      std::cout << std::format(form, std::forward<Args>(args)...) << '\n';
-    }
+  constexpr void println(std::string_view form, Args... args) {
+    std::cout << std::vformat(form, std::make_format_args(args...)) << '\n';
+  }
 }
